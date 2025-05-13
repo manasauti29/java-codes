bb7a66d89559d0ce7b19f2758f0dbe9500f2f742
Manas <manas.auti2909@gmail.com>
2025-05-13 10:45:12 +0530

    testing push with adding a testPrint file

---
+++ Java-Codes/src/BaseCodes/testPrint.java
@@ -0,0 +1,11 @@
+package BaseCodes;
+
+public class testPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test print");
+
+	}
+
+}
